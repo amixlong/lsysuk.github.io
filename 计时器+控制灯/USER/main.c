9bcb4102863f1a990dae17854ab024162591bdb6
amixlong <42435244+amixlong@users.noreply.github.com>
2019-03-05 21:06:05 +0800

    Update main.c
    test

--- 计时器+控制灯/USER/main.c
+++ 计时器+控制灯/USER/main.c
@@ -5,32 +5,32 @@
 #include "key.h"
 #include "timer.h"
 /************************************************
- ALIENTEK ������STM32F429������ʵ��3
- ����ʵ��-HAL�⺯����
- ����֧�֣�www.openedv.com
- �Ա����̣�http://eboard.taobao.com 
- ��ע΢�Ź���ƽ̨΢�źţ�"����ԭ��"����ѻ�ȡSTM32���ϡ�
- �������������ӿƼ����޹�˾  
- ���ߣ�����ԭ�� @ALIENTEK
+ ALIENTEK 阿波罗STM32F429开发板实验3
+ 串口实验-HAL库函数版
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
 ************************************************/
 
 
 int main(void)
 {
     	 
-    HAL_Init();                     //��ʼ��HAL��   
-    Stm32_Clock_Init(360,25,2,8);   //����ʱ��,180Mhz
-    delay_init(180);                //��ʼ����ʱ����
-    uart_init(115200);              //��ʼ��USART
-    LED_Init();                     //��ʼ��LED 
-    KEY_Init();                     //��ʼ������
+    HAL_Init();                     //初始化HAL库   
+    Stm32_Clock_Init(360,25,2,8);   //设置时钟,180Mhz
+    delay_init(180);                //初始化延时函数
+    uart_init(115200);              //初始化USART
+    LED_Init();                     //初始化LED 
+    KEY_Init();                     //初始化按键
 	 
 	 TIM3_Init(9999,8999);//1s
     while(1)
     {
 		 
 		
-       ControlLed();
+       ControlLed();//
 
     } 
 }
